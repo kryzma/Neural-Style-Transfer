db056da66e2170fdc0b6098fb15ead181d1a07b2
kryzma <48096081+kryzma@users.noreply.github.com>
2021-01-12 19:12:45 +0200

    Update style-transfer.py

--- style-transfer.py
+++ style-transfer.py
@@ -9,9 +9,13 @@ from torch.autograd import Variable
 
 def build_loss(neural_net, optimizing_img, target_content_representation, target_style_representation, content_feature_maps_name, style_feature_maps_names, config):
 
+	target_content_representation = target_content_representation.squeeze(axis=0)
+
 	current_set_of_feature_maps = neural_net(optimizing_img)
 
 	current_content_representation = current_set_of_feature_maps[content_feature_maps_name].squeeze(axis=0) # ?
+
+
 	content_loss = torch.nn.MSELoss(reduction='mean')(target_content_representation, current_content_representation)
 
 	style_loss = 0.0
@@ -29,7 +33,7 @@ def tuning_step(neural_net, optimizer, target_content_representation, target_sty
 
 	total_loss, content_loss, style_loss = build_loss(neural_net, optimizing_img, target_content_representation, target_style_representation, content_feature_maps_name, style_feature_maps_names, config)
 
-	total_loss.backward()
+	total_loss.backward(retain_graph=True)
 	optimizer.step()
 	optimizer.zero_grad()
 	return total_loss, content_loss, style_loss
@@ -61,7 +65,7 @@ def neural_style_transfer(config):
 	for cnt in range(config['num_of_iterations']):
 		total_loss, content_loss, style_loss = tuning_step(neural_net, optimizer, target_content_representation, target_style_representation, content_feature_maps_name, style_feature_maps_names, optimizing_img, config)
 		print(f"Iteration: {cnt:03}, tot loss={total_loss.item():9.5f}, content loss={config['content_weight']*content_loss.item():9.5f}, style loss={config['style_weight']*style_loss.item():9.5f}")
-		if(cnt % 20 == 0):
+		if(cnt % 10 == 0):
 			utils.save_image(optimizing_img, cnt, config)
 
 
@@ -71,19 +75,19 @@ if __name__ == "__main__":
 	config['output_dir'] = "C:/Users/Mantas/Desktop/StyleTransfer/images/results/"
 
 	config['content_images_dir'] = "C:/Users/Mantas/Desktop/StyleTransfer/images/contents/"
-	config['content_image_name'] = "noise.jpg"
+	config['content_image_name'] = "me.jpg"
 	config['style_images_dir'] = "C:/Users/Mantas/Desktop/StyleTransfer/images/styles/"
 	config['style_image_name'] = "tomato.jpg"
 	config['init_images_dir'] = "C:/Users/Mantas/Desktop/StyleTransfer/images/contents/"
-	config['init_image_name'] = "noise.jpg"
+	config['init_image_name'] = "me.jpg"
 
 	config['height'] = 256
 
 	config['num_of_iterations'] = 1000
-	config['learning_rate'] = 1e2 # 1e1
+	config['learning_rate'] = 1e1 # 1e1
 
-	config['content_weight'] = 0 # 1e5
-	config['style_weight'] = 2e5 # 1e5
+	config['content_weight'] = 1e5 # 1e5
+	config['style_weight'] = 1e5 # 1e5
 
 	neural_style_transfer(config)
 
