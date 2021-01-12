35c6f3b981b9da2f8b3dce4388d4fc0da2a0b905
kryzma <48096081+kryzma@users.noreply.github.com>
2021-01-12 19:12:31 +0200

    Update network.py

--- network.py
+++ network.py
@@ -13,48 +13,28 @@ class Vgg19(torch.nn.Module):
 		vgg_pretrained_features = models.vgg19(pretrained=True).features
 		# Only these layers are used for style transfer
 		self.layers_names = ['conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv4_2', 'conv5_1']
+		self.used_layers_cnt = len(self.layers_names)
 		self.content_feature_maps_index = 4;
 		self.style_feature_maps_indices = [0, 1, 2, 3, 5]
 
-		self.slice1 = torch.nn.Sequential()
-		self.slice2 = torch.nn.Sequential()
-		self.slice3 = torch.nn.Sequential()
-		self.slice4 = torch.nn.Sequential()
-		self.slice5 = torch.nn.Sequential()
-		self.slice6 = torch.nn.Sequential()
+		self.slice_range = [0,1,6,11,20,22,29]
+		self.slices = [ torch.nn.Sequential() for i in range(self.used_layers_cnt) ]
 
-		for x in range(1):
-			self.slice1.add_module(str(x), vgg_pretrained_features[x])
-		for x in range(1, 6):
-			self.slice2.add_module(str(x), vgg_pretrained_features[x])
-		for x in range(6, 11):
-			self.slice3.add_module(str(x), vgg_pretrained_features[x])
-		for x in range(11, 20):
-			self.slice4.add_module(str(x), vgg_pretrained_features[x])
-		for x in range(20, 22):
-			self.slice5.add_module(str(x), vgg_pretrained_features[x])
-		for x in range(22, 29):
-			self.slice6.add_module(str(x), vgg_pretrained_features[x])
+		for slice_ind in range(self.used_layers_cnt):
+			for layer_ind in range(self.slice_range[slice_ind], self.slice_range[slice_ind+1]):
+				self.slices[slice_ind].add_module(str(layer_ind), vgg_pretrained_features[layer_ind])
 		
 		# do not use autograd for parameters
 		for param in self.parameters():
 			for param in self.parameters():
 				param.requires_grad = False
 
+
 	def forward(self, x):
 		results = []
-		x = self.slice1(x)
-		results.append(x)
-		x = self.slice2(x)
-		results.append(x)
-		x = self.slice3(x)
-		results.append(x)
-		x = self.slice4(x)
-		results.append(x)
-		x = self.slice5(x)
-		results.append(x)
-		x = self.slice6(x)
-		results.append(x)
+		for slice_ind in range(self.used_layers_cnt):
+			x = self.slices[slice_ind](x)
+			results.append(x)
 
 		out = dict(zip(self.layers_names, results))
 		return out
