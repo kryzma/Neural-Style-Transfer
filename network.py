aace840f28b196d13ca696be9cd440d28db6a7e2
kryzma <48096081+kryzma@users.noreply.github.com>
2021-01-13 09:58:30 +0200

    Changed MaxPool2d into AvgPool2d
    Did as suggested in the paper

--- network.py
+++ network.py
@@ -9,8 +9,6 @@ class Vgg19(torch.nn.Module):
 
 	def __init__(self):
 		super().__init__()
-		# Gives list of layers with weights
-		vgg_pretrained_features = models.vgg19(pretrained=True).features
 		# Only these layers are used for style transfer
 		self.layers_names = ['conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv4_2', 'conv5_1']
 		self.used_layers_cnt = len(self.layers_names)
@@ -18,12 +16,10 @@ class Vgg19(torch.nn.Module):
 		self.style_feature_maps_indices = [0, 1, 2, 3, 5]
 
 		self.slice_range = [0,1,6,11,20,22,29]
-		self.slices = [ torch.nn.Sequential() for i in range(self.used_layers_cnt) ]
-
-		for slice_ind in range(self.used_layers_cnt):
-			for layer_ind in range(self.slice_range[slice_ind], self.slice_range[slice_ind+1]):
-				self.slices[slice_ind].add_module(str(layer_ind), vgg_pretrained_features[layer_ind])
+		self.all_slices = [ torch.nn.Sequential() for i in range(self.used_layers_cnt) ]
 		
+		self.construct_slices()
+
 		# do not use autograd for parameters
 		for param in self.parameters():
 			for param in self.parameters():
@@ -33,8 +29,19 @@ class Vgg19(torch.nn.Module):
 	def forward(self, x):
 		results = []
 		for slice_ind in range(self.used_layers_cnt):
-			x = self.slices[slice_ind](x)
+			x = self.all_slices[slice_ind](x)
 			results.append(x)
 
 		out = dict(zip(self.layers_names, results))
 		return out
+
+
+	def construct_slices(self):
+		vgg_pretrained_features = models.vgg19(pretrained=True).features
+		
+		for slice_ind in range(self.used_layers_cnt):
+			for layer_ind in range(self.slice_range[slice_ind], self.slice_range[slice_ind+1]):
+				if vgg_pretrained_features[layer_ind].__class__.__name__ == 'MaxPool2d':
+					self.all_slices[slice_ind].add_module(str(layer_ind), torch.nn.AvgPool2d(kernel_size=2, stride=2, padding=0, ceil_mode=False))
+				else:
+					self.all_slices[slice_ind].add_module(str(layer_ind), vgg_pretrained_features[layer_ind])
